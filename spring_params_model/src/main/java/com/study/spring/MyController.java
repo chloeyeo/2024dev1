34b1dc52e19c22cde99c5b1af74012e7f57ce237
chloeyeo <yeochloe01@gmail.com>
2024-06-21 10:41:23 +0900

    using path variable

--- spring_params_model/src/main/java/com/study/spring/MyController.java
+++ spring_params_model/src/main/java/com/study/spring/MyController.java
@@ -1,8 +1,11 @@
 package com.study.spring;
 
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -23,5 +26,28 @@ public class MyController {
 		model.addAttribute("name", name);
 		model.addAttribute("id", id);
 		return "index";
+	}
+	
+	@RequestMapping("/test1")
+	public String test1(@RequestParam("id") String id, @RequestParam String name, Model model) {
+		model.addAttribute("name", name);
+		model.addAttribute("id", id);
+		return "test1";
+	}
+	
+	@RequestMapping("/test2")
+	public String test2(Member member, Model model) { // Member is a DTO
+		member.setName("chloe");
+		member.setId("1234");
+		model.addAttribute("name", member.getName());
+		model.addAttribute("id", member.getId());
+		return "test2";
+	}
+	
+	@RequestMapping("/test3/{name}/{id}")
+	public String test3(Model model, @PathVariable("name") String name, @PathVariable("id") String id) {
+		model.addAttribute("name", name);
+		model.addAttribute("id", id);
+		return "test3";
 	}
 }
---
+++ spring_params_model/src/main/webapp/WEB-INF/views/test1.jsp
@@ -0,0 +1,13 @@
+<%@ taglib uri="jakarta.tags.core" prefix="c" %> <%@ page language="java"
+contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>test1 JSP file</title>
+  </head>
+  <body>
+    <h2>안녕하세요, 테스트1입니다.</h2>
+    당신의 이름은 ${name} 입니다. ID는: ${id}
+  </body>
+</html>
