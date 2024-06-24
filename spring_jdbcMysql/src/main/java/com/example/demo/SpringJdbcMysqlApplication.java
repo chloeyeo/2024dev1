f5b2ff49d03bc8ceec0811a59e02ccae8b2fcd37
chloeyeo <yeochloe01@gmail.com>
2024-06-24 12:20:10 +0900

    dto,dao

---
+++ spring_jdbcMysql/src/main/java/com/example/demo/SpringJdbcMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcMysqlApplication.class, args);
+	}
+
+}
