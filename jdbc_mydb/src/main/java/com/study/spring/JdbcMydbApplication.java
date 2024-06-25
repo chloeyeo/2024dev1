e6a8ca6f9a11180269cf70ccbdb4b874f49a6e77
chloeyeo <yeochloe01@gmail.com>
2024-06-25 11:34:14 +0900

    get single dto by id then view

---
+++ jdbc_mydb/src/main/java/com/study/spring/JdbcMydbApplication.java
@@ -0,0 +1,13 @@
+package com.study.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcMydbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcMydbApplication.class, args);
+	}
+
+}
