d221a3c5c0fcb9287a510bfc6f55a6bd08af6839
CDL <CDL@host.docker.internal>
2024-07-02 13:18:38 +0900

    jpa builder

--- 12_jpa02_crud/src/main/java/com/study/spring/jpa/MyController.java
+++ 12_jpa02_crud/src/main/java/com/study/spring/jpa/MyController.java
@@ -1,13 +1,14 @@
 package com.study.spring.jpa;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 public class MyController {
@@ -22,15 +23,17 @@ public class MyController {
 	@PostMapping("/insert") // insert?username=han
 	public String insert(@RequestParam("username") String username, @RequestParam("content") String content, Model model) {
 		
+		// Method 1 - faster speed
 //		Member user = new Member();
 //		user.changeUsername(username);
 //		user.changeCreateDate(LocalDate.now());
 		
+		// Method 2 - more convenient as it automatically creates constructors
 		Member user = Member.builder()
 				.username(username)
 				.content(content)
 				.createDate(LocalDate.now())
-				.build(); // SAME as 3 commented lines above
+				.build(); // SAME as Method 1
 		
 		//Member user = Member.builder().username(username).build(); // this ALSO works with builder()
 		// even if there's no single parameter constructor in Member class - builder() automatically creates this constructor
@@ -41,4 +44,36 @@ public class MyController {
 		
 		return "insert"; // returns insert.jsp
 	}
+	
+	@GetMapping("/selectall")
+	public String selectAll(Model model) {
+		List<Member> allMembers = memberService.selectAll();
+		model.addAttribute("allMembers", allMembers);
+		return "selectAll"; // selectAll.jsp
+	}
+	
+	@GetMapping("/select") // select?id=1
+	public String select(@RequestParam("id") Long id, Model model) {
+		Optional<Member> result =  memberService.select(id);
+		if (result.isPresent()) {
+			model.addAttribute("member", result.get());
+		} else {
+			model.addAttribute("member", null);
+		}
+		return "select"; // select.jsp
+	}
+	
+	@DeleteMapping("/delete")
+	public String delete(@RequestParam("id") Long id) {
+		memberService.delete(id);
+		return "delete";
+	}
+	
+	@PutMapping("/update")
+	public String update(Member member, Model model) {
+		Member result = memberService.update(member);
+		model.addAttribute("member", result);
+		return "update";
+	}
+	
 }
